e7b48e838210d4ae8cd0becf00c2cfa8898c562a
tianwei <liutianweidlut@gmail.com>
2023-07-18 18:39:19 +0800

    remove wip redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import React from "react";
 import { Redirect } from "@docusaurus/router"
 
 const Home = () => {
-    return <Redirect to="/next" />;
+    return <Redirect to="/" />;
 }
 
 export default Home;
